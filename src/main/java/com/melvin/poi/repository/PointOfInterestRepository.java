5964de3d53494f27f42727a193814e2e223c86cd
melvintivane <melvintivane@gmail.com>
2025-03-27 20:12:36 +0200

    refactor: migrate repository logic to service layer and add pagination
    - Replaced direct repository calls in  with
    - Updated  method to support pagination
    - Introduced  DTO for structured API responses
    - Enabled SQL logging and Hibernate formatting in

--- src/main/java/com/melvin/poi/repository/PointOfInterestRepository.java
+++ src/main/java/com/melvin/poi/repository/PointOfInterestRepository.java
@@ -1,6 +1,8 @@
 package com.melvin.poi.repository;
 
 import com.melvin.poi.entity.PointOfInterest;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
@@ -13,8 +15,9 @@ public interface PointOfInterestRepository extends JpaRepository<PointOfInterest
         SELECT p FROM PointOfInterest p
         WHERE (p.x >= :xMin AND p.x <= :xMax AND p.y >= :yMin AND p.y <= :yMax)
         """)
-   List<PointOfInterest> findNearMe(@Param("xMin") long xMin,
+   Page<PointOfInterest> findNearMe(@Param("xMin") long xMin,
                                     @Param("xMax") long xMax,
                                     @Param("yMin") long yMin,
-                                    @Param("yMax") long yMax);
+                                    @Param("yMax") long yMax,
+                                    Pageable pageable);
 }
